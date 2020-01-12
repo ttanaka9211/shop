eae0a280080c73a3c7b919afffde65c93ee3d726
田中利幸 <ttanaka@hatchdogs.net>
2020-01-12 21:50:59 +0900

    追加

---
+++ kanri/insert.php
@@ -0,0 +1,19 @@
+<?php
+    require 'common.php';
+    $error = $name = $comment = $price = '';
+    $pdo = connect();
+    if (@$_POST['submit']) {
+        $name = $_POST['name'];
+        $comment = $_POST['comment'];
+        $price = $_POST['price'];
+        if (!$name) $error .= '商品がありません。<br>';
+        if (!$comment) $error .= '商品説明がありません。<br>';
+        if (!$price) $error .= '価格が有りません。<br>';
+        if (preg_match('/\D/',$price)) $error .= '価格が不正です。<br>';
+        if (!$error) {
+            $pdo->query("INSERT INTO goods(name,comment,price) VALUES('$name','$comment','$price')");
+            header('Location: index.php');
+            exit();
+        }
+    }
+    require 't_insert.php';
