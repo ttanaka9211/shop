11ed309a862e54536506183a2da91178d6d3fabf
田中利幸 <ttanaka@hatchdogs.net>
2020-01-13 17:59:32 +0900

    エラー修正

--- kanri/index.php
+++ kanri/index.php
@@ -1,6 +1,6 @@
 <?php
 require 'common.php';
 $pdo = connect();
-$st = $pdo->query("SELECT * FORM goods");
+$st = $pdo->query("SELECT * FROM goods");
 $goods = $st->fetchAll();
 require 't_index.php';
